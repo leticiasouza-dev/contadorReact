d75cbe3bca5d1cd51b49cc7776789c6711505ac7
leticiaSouzaDev <lec667121@gmail.com>
2024-02-20 17:59:05 -0300

    feat: iniciando a estilização do contador

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,17 @@ function App() {
   const [contador, setContador] = useState(0);
 
   return (
-    <div>
+    <main>
+      <div className='container'>
         <h1>Contador</h1>
 
         <p>{contador}</p>
-        <button onClick={() => setContador(contador + 1)}>Adicionar</button>
-        <button onClick={() => setContador(() => {contador <= 0 ? setContador(0) : setContador(contador - 1)})}>Diminuir</button> {/*definindo que meu contador ao diminuir só vai até o 0 */}
+        <button onClick={() => setContador(contador + 1)} className='bottaoAdd'>Adicionar</button>
+        <button onClick={() => setContador(() => {contador <= 0 ? setContador(0) : setContador(contador - 1)})} className='bottaoDiminuir'>Diminuir</button> {/*definindo que meu contador ao diminuir só vai até o 0 */}
         <button onClick={() => setContador(0)}>Zerar</button>
     </div>
+    </main>
+    
       
   )
 }
